9db9e135ac698b2adbb7cd60ba1d693f7ef03a38
Thomas Rice <59601109+Anubliss-0@users.noreply.github.com>
2024-09-15 15:32:49 +0100

    testing gone

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  test: {
-    // 👋 add the line below to add jsdom to vite
-    environment: 'jsdom',
-  }
+  plugins: [react()]
 })
